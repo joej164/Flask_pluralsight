b3436191d64568781257d01d02ec28978c00d9e4
Joseph Jacobs <joej164@gmail.com>
2017-07-16 22:08:44 -0700

    additional updates to the HTML and CSS files

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,11 +1,63 @@
-from flask import Flask, render_template, url_for
+import os
+from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+
+from thermos.forms import BookmarkForm
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'h\xb3\xbbE\xf4\x82ZRB\xee$\xac\x98\x1c7\xa0l\x1aCO\xed\xba\xd1\x8a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+db = SQLAlchemy(app)
+
+bookmarks = []
+
+
+def store_bookmark(url, description):
+    bookmarks.append(dict(
+        url=url,
+        description=description,
+        user="joemama",
+        date=datetime.utcnow()
+    ))
+
+
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           title="Title for Section 2",
+                           text={"key": "value", "key1": "value1"},
+                           new_bookmarks=new_bookmarks(5))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored '{}'".format(description))
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
